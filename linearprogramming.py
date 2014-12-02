2e1ab80edde40d5ba88e84621ec5818750c74360
Cezary Wojcik <cezarywojcik92@gmail.com>
2014-12-01 22:59:38 -0800

    Initial Commit

---
+++ linearprogramming.py
@@ -0,0 +1,13 @@
+# Problem 1
+from pylpsolve import LP
+lp = LP()
+lp.setObjective([4, 12, 7, 8, 14, 11, 4, 13, 9], mode="maximize")
+lp.addConstraint([[1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1],
+  [0, 1, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 1, 0, 0, 1, 0, 0, 1]],
+  "<=",
+  [400, 480, 230, 420, 250])
+lp.solve()
+print lp.getSolution()
